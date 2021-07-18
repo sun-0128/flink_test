b642d6f7908cfc3ffcaafc02a5e13c03ed92f71e
sun-0128 <1335902858@qq.com>
2021-07-18 09:16:12 +0800

    first

--- src/main/java/com/guigu/flink/utils/rcmflink.sh
+++ src/main/java/com/guigu/flink/utils/rcmflink.sh
@@ -1,1 +1,1 @@
-flink run -m yarn-cluster  -ynm test_redis   -yt  /data/swx1007553/     -yjm 1024 -ytm 4096   -c com.guigu.flink.TestParameterTools    /data/swx1007553/test0717.jar --configFile /data/swx1007553/config.properties
+flink run -m yarn-cluster   -ynm test_redis   -yjm 1024 -ytm 1024   -c com.guigu.flink.TestParameterTools    /data/swx1007553/test0717.jar --configFile   /data/swx1007553/config.properties
